3dcb8d6fc63d78f076cfdbd1476a911a5571c6f5
ronathan <ronaldvictorino@gmail.com>
2016-04-06 00:11:00 -0400

    Full website complete. Somewhat responsive.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
-$(function() {
+(function() {
+	$('section#birthday-hero .section-content').addClass("animated fadeInUp");
 
-});
+	var birthdayLanding = $('section#birthday-landing').waypoint({
+		handler: function(direction) {
+			$('section#birthday-landing .section-content').removeClass("hidden").addClass("animated fadeInRight");
+			$('section#birthday-landing .section-image').removeClass("hidden").addClass("animated fadeInLeft");
+		},
+		offset: "60%"
+	});
+
+	var birthdayFeatures = $('section#birthday-features').waypoint({
+		handler: function(direction) {
+			$('section#birthday-features .section-content').removeClass("hidden").addClass("animated fadeInRight");
+			$('section#birthday-features .section-image').removeClass("hidden").addClass("animated fadeInLeft");
+		},
+		offset: "60%"
+	});
+
+	var birthdayDetails = $('section#birthday-details').waypoint({
+		handler: function(direction) {
+			$('section#birthday-details .section-content').removeClass("hidden").addClass("animated fadeInLeft");
+			$('section#birthday-details .section-image').removeClass("hidden").addClass("animated fadeInRight");
+		},
+		offset: "60%"
+	});
+
+	var birthdayClosing = $('section#birthday-closing').waypoint({
+		handler: function(direction) {
+			$('section#birthday-closing .section-content').removeClass("hidden").addClass("animated fadeInUp");
+		},
+		offset: "60%"
+	});
+})();
